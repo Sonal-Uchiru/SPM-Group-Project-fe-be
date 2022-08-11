8d6f0d17fbe72d485997e70674f6b2ceb0584245
ChamodhIdurange <89272288+ChamodhIdurange@users.noreply.github.com>
2022-08-11 23:02:31 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React, {Component} from 'react';
 import Login from "./components/authentication/loginPage";
+import ProfileMain from "./components/cards/userProfileMain";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login/>}/>
+                <Route path="/" element={<ProfileMain/>}/>
             </Routes>
         </Router>
     );
