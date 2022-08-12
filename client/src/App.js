a78ea43f6d7c8e0e51016e102b96656f9732c76e
kavee <IT20185770@my.sliit.lk>
2022-08-12 12:13:56 +0530

    View All Jobs(User) Page Card Ui Design Completed

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,14 @@ import Footer from "./components/Footer/footer";
 import UserHeader from "./components/user/navigation/userHeader";
 import CompanyHeader from "./components/company/navigation/companyHeader";
 import AdminHeader from "./components/admin/navigation/adminHeader";
+import AppliedJobCard from "./components/user/cards/appliedJobCard";
 
 function App() {
     return (
         <Router>
-            <AdminHeader/>
+            {/*<AdminHeader/>*/}
             <Routes>
-                <Route path="/" element={<Login/>}/>
+                <Route path="/" element={<AppliedJobCard/>}/>
             </Routes>
             {/*<Footer/>*/}
         </Router>
