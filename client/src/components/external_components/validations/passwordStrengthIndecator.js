38117d043f9ae01022811770e5aea54a87a44ebf
sonaljayawardana <sonal123326@gmail.com>
2022-08-26 11:47:00 +0530

    password complexity checker implemented

--- client/src/components/external_components/validations/passwordStrengthIndecator.js
+++ client/src/components/external_components/validations/passwordStrengthIndecator.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import zxcvbn from 'zxcvbn';
+import React from 'react'
+import PasswordValidator from "password-validator"
 
-const PasswordStrengthMeter = ({password}) => {
-    const testResult = zxcvbn(password);
-    const num = testResult.score * 100 / 4;
+export const PasswordStrengthMeter = ({password}) => {
+
+    const num = 100 - (getScore(password) * 100 / 4)
 
     const createPassLabel = () => {
-        switch (testResult.score) {
-            case 0:
-                return 'Very weak';
-            case 1:
-                return 'Weak';
-            case 2:
-                return 'Fear';
-            case 3:
-                return 'Good';
-            case 4:
-                return 'Strong';
-            default:
-                return '';
-        }
+        const score = getScore(password)
+
+        if (score === 4) return 'Very weak'
+
+        if (score === 3) return 'Weak'
+
+        if (score === 2) return 'Fear'
+
+        if (score === 1) return 'Good'
+
+        if (score === 0) return 'Strong'
+
+        return ''
     }
 
     const funcProgressColor = () => {
-        switch (testResult.score) {
-            case 0:
-                return '#828282';
-            case 1:
-                return '#EA1111';
-            case 2:
-                return '#FFAD00';
-            case 3:
-                return '#9bc158';
-            case 4:
-                return '#00b500';
-            default:
-                return 'none';
-        }
+        const score = getScore(password)
+
+        if (score === 4) return '#828282'
+
+        if (score === 3) return '#EA1111'
+
+        if (score === 2) return '#FFAD00'
+
+        if (score === 1) return '#9bc158'
+
+        if (score === 0) return '#00b500'
+
+        return 'none'
     }
 
     const changePasswordColor = () => ({
@@ -55,4 +53,25 @@ const PasswordStrengthMeter = ({password}) => {
     )
 }
 
-export default PasswordStrengthMeter
+const getScore = (password) => {
+    const schema = new PasswordValidator();
+
+    schema
+        .is().min(8)     // Minimum length 8
+        .is().max(100)  // Maximum length 100
+        .has().uppercase()   // Must have uppercase letters
+        .has().lowercase()   // Must have lowercase letters
+        .has().digits(2)  // Must have at least 2 digits
+        .has().not().spaces()    // Should not have spaces
+        .is().not().oneOf(['Passw0rd', 'Password123', 'Email123']);
+
+    return schema.validate(password, {details: true}).length
+}
+
+export const isStrongPassword = (password) => {
+    return getScore(password) === 0
+}
+
+
+
+
