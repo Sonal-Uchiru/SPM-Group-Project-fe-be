27699f050f9f4d3c4871b787c82e66b24b70a569
sonaljayawardana <sonal123326@gmail.com>
2022-09-01 12:25:23 +0530

    List all job applications for a specific job (Admin) implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React from "react";
 import CompanyHeader from "./components/managements/company/navigation/companyHeader";
 import Footer from "./components/external_components/navigation/footer";
@@ -6,16 +6,18 @@ import JobApplicationForm from "./components/managements/jobApplications/jobAppl
 import ViewAppliedJobs from "./components/managements/jobApplications/viewAppliedJobs";
 import ViewAllJobsCompany from "./components/managements/jobs/pages/viewAllJobsCompany";
 import ViewAllJobs from "./components/managements/jobs/pages/viewAllJobs";
+import CompanyList from "./components/managements/admin/data_tables/companyList";
+import AllJobApplications from "./components/managements/admin/data_tables/listAllAppliedJobsApplications";
 
 function App() {
-  return (
-    <Router>
-      <CompanyHeader />
-      <Routes>
-        <Route path="/" element={<ViewAppliedJobs/>}/>
-      </Routes>
-      <Footer />
-    </Router>
+    return (
+        <Router>
+            <CompanyHeader/>
+            <Routes>
+                <Route path="/" element={<AllJobApplications/>}/>
+            </Routes>
+            <Footer/>
+        </Router>
   );
 }
 
--- client/src/components/managements/admin/data_tables/listAllAppliedJobsApplications.js
+++ client/src/components/managements/admin/data_tables/listAllAppliedJobsApplications.js
@@ -1,93 +1,109 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "datatables.net-dt/js/dataTables.dataTables";
 import "datatables.net-dt/css/jquery.dataTables.min.css";
 import $ from "jquery";
 import "../css/listAllJobApplications.css";
 import SummaryCard from "../cards/summaryCard";
+import {getAppliedJobApplicationsByJobId} from "../../../../api/managements/jobApplicationApi";
+import {ErrorAlert} from "../../../../sweet_alerts/error";
+import moment from "moment";
+import {getJobById} from "../../../../api/managements/jobApi";
 
 export default function AllJobApplications() {
-  $(document).ready(function () {
-    $("#allJobApplicationsTable").DataTable();
-  });
+    const [jobApplications, setJobApplications] = useState([])
+    const [job, setJob] = useState([])
+    const [selectedJobApplications, setSelectedJobApplications] = useState(0)
+    const [rejectedJobApplications, setRejectedJobApplications] = useState(0)
 
-  return (
-    <div className="allJobApplications">
-      <h2 className="pageTitle">
-        <i className="fa fa-arrow-left" />
-        Job Applications (Senior Software Engineer (Frontend Developer))
-      </h2>
-      <div className="row d-flex justify-content-center">
-        <div className="col-md-4">
-          <SummaryCard topic="Selected Applications" count="40" />
-        </div>
-        <div className="col-md-4">
-          <SummaryCard topic="Rejected Applications" count="30" />
-        </div>
-      </div>
-      <div className="report">
-        <button type="button" className="btn btn-primary downloadReportButton">
-          <i className="fa fa-download" />
-          Download Report
-        </button>
-      </div>
-      <div className="col-md-12 job-applications-table-div">
-        <div className="scrollbar">
-          <table
-            id="allJobApplicationsTable"
-            className="table table-bordered table-sm nowrap table-hover job-applications-table"
-          >
-            <thead>
-              <tr>
-                <th />
-                <th>Name</th>
-                <th>Gender</th>
-                <th>Mobile</th>
-                <th>Applied Date</th>
-                <th>Last Updated Date</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <img src="./images/user.jpg" className="tableImg" alt="" />
-                </td>
-                <td>Nimal Perera</td>
-                <td>Male</td>
-                <td>0768899283</td>
-                <td>18/12/2012, 03:00:00</td>
-                <td>20/12/2012, 03:00:00</td>
-                <td>
-                  <img
-                    src="./../images/editing.png"
-                    className="tableEdit"
-                    alt=""
-                  />
-                </td>
-              </tr>
+    const jobId = "62f9e781d06c6643a5f74e69";
+    const userProfilePlaceHolder = "https://firebasestorage.googleapis.com/v0/b/moon-cinema-rest-api.appspot.com/o/Additional%2Fuser%20(8).png?alt=media&token=9cef4e9b-1e8c-43ca-95b7-19c6e9ec8781"
 
-              <tr>
-                <td>
-                  <img src="./images/user.jpg" className="tableImg" alt="" />
-                </td>
-                <td>Nimal Perera</td>
-                <td>Male</td>
-                <td>0768899283</td>
-                <td>18/12/2012, 03:00:00</td>
-                <td>20/12/2012, 03:00:00</td>
-                <td>
-                  <img
-                    src="./../images/editing.png"
-                    className="tableEdit"
-                    alt=""
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+    useEffect(() => {
+        getAppliedJobApplicationsByJobId(jobId).then((res) => {
+            if (res.data.content) {
+                setJobApplications(res.data.content)
+                setSelectedJobApplications(jobApplicationsCount(res.data.content, 1))
+                setRejectedJobApplications(jobApplicationsCount(res.data.content, 2))
+                $(document).ready(function () {
+                    $("#allJobApplicationsTable").DataTable();
+                });
+            }
+        }).catch(async (err) => {
+            console.log(err)
+            await ErrorAlert('Something went wrong!')
+        })
+    }, [])
+
+
+    useEffect(() => {
+        getJobById(jobId).then((res) => {
+            if (res.data) setJob(res.data)
+        }).catch(async (err) => {
+            console.log(err)
+            await ErrorAlert('Something went wrong!')
+        })
+    }, [])
+
+    const jobApplicationsCount = (arr, status) => {
+        return arr.filter(obj => {
+            return obj.status === status;
+        }).length;
+    }
+
+    return (
+        <div className="allJobApplications">
+            <h2 className="pageTitle">
+                <i className="fa fa-arrow-left"/>
+                Job Applications ({`${job.position} (${job.developmentArea})`})
+            </h2>
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-4">
+                    <SummaryCard topic="Selected Applications" count={selectedJobApplications}/>
+                </div>
+                <div className="col-md-4">
+                    <SummaryCard topic="Rejected Applications" count={rejectedJobApplications}/>
+                </div>
+            </div>
+            <div className="col-md-12 job-applications-table-div">
+                <div className="scrollbar">
+                    <table
+                        id="allJobApplicationsTable"
+                        className="table table-bordered table-sm nowrap table-hover job-applications-table"
+                    >
+                        <thead>
+                        <tr>
+                            <th/>
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Mobile</th>
+                            <th>Applied Date</th>
+                            <th>Last Updated Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {jobApplications.map((jobApplication, index) => {
+                            return (
+                                <>
+                                    <tr key={index}>
+                                        <td>
+                                            <img
+                                                src={!jobApplication.userDetails[0].profilePicture ? userProfilePlaceHolder : jobApplication.userDetails[0].profilePicture}
+                                                className="tableImg" alt=""/>
+                                        </td>
+                                        <td>{`${jobApplication.userDetails[0].firstName} ${jobApplication.userDetails[0].lastName}`}</td>
+                                        <td>{!jobApplication.userDetails[0].gender ? jobApplication.applicantOtherDetails.gender : jobApplication.userDetails[0].gender}</td>
+                                        <td>{`0${jobApplication.userDetails[0].mobile}`}</td>
+                                        <td>{moment(jobApplication.createdDate).format('YYYY-MM-DD HH:mm:ss')}</td>
+                                        <td>{jobApplication.updatedDate ? moment(jobApplication.updatedDate).format('YYYY-MM-DD HH:mm:ss') : 'Not Updated!'}</td>
+                                    </tr>
+                                </>
+                            )
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <br/>
         </div>
-      </div>
-      <br />
-    </div>
-  );
+    );
 }
