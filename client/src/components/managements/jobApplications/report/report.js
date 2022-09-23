7147cfff1afd1ca895eb9dad77711dce5179d9c8
kavee <IT20185770@my.sliit.lk>
2022-09-23 23:40:56 +0530

    Report Template UI Design Patially Corrected

--- client/src/components/managements/jobApplications/report/report.js
+++ client/src/components/managements/jobApplications/report/report.js
@@ -6,40 +6,121 @@ import ReportFooter from "./reportFooter";
 export default function Report({tableHeaders = [], tableMetaData = [], companyDetails, summaryCards = []}) {
     return (
         <>
-            <ReportHeader companyDetails={companyDetails} summaryCards={summaryCards}/>
-            <div className="job-applications-table-div">
-                <div className="container-fluid">
-                    <table
-                        id="allJobApplicationsTable"
-                        className="table table-bordered table-sm nowrap job-applications-table"
-                    >
-                        <thead>
-                        <tr>
-                            {tableHeaders && tableHeaders.map(th => {
-                                return (
-                                    <th>{th}</th>
-                                )
-                            })}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {tableMetaData &&
-                            tableMetaData.map(data => {
-                                return (
-                                    <tr>
-                                        <td>fjjfk</td>
-                                        <td>fjjfk</td>
-                                        <td>fjjfk</td>
-                                        <td>fjjfk</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                        </tbody>
-                    </table>
+            <div className="page">
+                <div className="subpage">
+                    <ReportFooter/>
+                    <ReportHeader companyDetails={companyDetails} summaryCards={summaryCards}/>
+                    <div className="job-applications-table-div">
+                        <div className="container-fluid">
+                            <table
+                                id="allJobApplicationsTable"
+                                className="table table-bordered table-sm nowrap job-applications-table"
+                            >
+                                <thead>
+                                <tr>
+                                    {/*{tableHeaders && tableHeaders.map(th => {*/}
+                                    {/*    return (*/}
+                                    <th>No</th>
+                                    <th>Position</th>
+                                    <th>Development Area</th>
+                                    <th>Job Type</th>
+                                    <th>Created Date</th>
+                                    <th>Last Updated Date</th>
+
+                                    {/*// {th}*/}
+                                    {/*    )*/}
+                                    {/*})}*/}
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {/*{tableMetaData &&*/}
+                                {/*    tableMetaData.map(data => {*/}
+                                {/*        return (*/}
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+
+                                <tr>
+                                    <td>1</td>
+                                    <td>Senior Software Engineer</td>
+                                    <td>Frontend</td>
+                                    <td>Full time</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                    <td>20/12/2012, 03:00:00</td>
+                                </tr>
+
+
+                                {/*        )*/}
+                                {/*    })*/}
+                                {/*}*/}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <ReportFooter/>
         </>
     )
 }
--- client/src/components/managements/jobApplications/report/reportFooter.js
+++ client/src/components/managements/jobApplications/report/reportFooter.js
@@ -6,7 +6,7 @@ function ReportFooter(props) {
         <div className="company-report">
             <div className="text-center site-logo">
                 <img
-                    src="./images/SPMNEW.png" width="160" height="60"
+                    src="./images/SPMNEW.png" width="160" height="40"
                     className="img-fluid logo"
                     alt="site logo"
                 />
