ca122a9d70c739f9bf93b9b52658d4d27c242052
ChamodhIdurange <89272288+ChamodhIdurange@users.noreply.github.com>
2022-10-11 22:47:28 +0530

    Small Issues fixed

--- client/src/api/managements/jobApi.js
+++ client/src/api/managements/jobApi.js
@@ -2,29 +2,33 @@ import { protectedApi } from "../protectedApi";
 import { ErrorAlert } from "../../sweet_alerts/error";
 
 export const getAllJobsByToken = async () => {
-    return await protectedApi("get", "jobs");
+  return await protectedApi("get", "jobs");
 };
 
 export const getAllJobs = async () => {
-    return await protectedApi("get", "jobs/list");
+  return await protectedApi("get", "jobs/list");
 };
 
 export const getJobById = async (id) => {
-    return await protectedApi("get", `jobs/${id}`);
+  return await protectedApi("get", `jobs/${id}`);
 };
 
 export const addNewJobs = async (data) => {
-    return await protectedApi("POST", "jobs", data);
+  return await protectedApi("POST", "jobs", data);
 };
 
 export const getJobsApplicants = async (companyId) => {
-    return await protectedApi("GET", `jobs/companies/summary/${companyId}`);
+  return await protectedApi("GET", `jobs/companies/summary/${companyId}`);
 };
 
 export const deleteJob = async (jobId) => {
-    return await protectedApi("DELETE", `jobs/${jobId}`);
+  return await protectedApi("DELETE", `jobs/${jobId}`);
 };
 
 export const getCompanyDataForJob = async () => {
-    return await protectedApi("GET", "companies");
+  return await protectedApi("GET", "companies");
+};
+
+export const getByCompany = async (companyId) => {
+  return await protectedApi("GET", `jobs/getByCompany/${companyId}`);
 };
